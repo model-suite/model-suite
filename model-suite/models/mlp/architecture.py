9559a26009e9c7ea9ea95037e71496081472549b
osbm <osmanfbayram@gmail.com>
2023-08-11 16:51:14 +0300

    add multi layer perceptron

---
+++ model-suite/models/mlp/architecture.py
@@ -0,0 +1,45 @@
+import torch
+import model_config
+from torch import nn
+
+class MultiLayerPerceptron(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        # Create a list to hold the layers of the MLP
+        layers = []
+        if config.hidden_layers:
+            # Add the input layer
+            layers.append(nn.Linear(config.input_shape, config.hidden_layers[0]))
+            if config.batch_normalization:
+                layers.append(nn.BatchNorm1d(config.hidden_layers[0]))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(config.dropout_rate))
+
+            # Add hidden layers
+            for i in range(len(config.hidden_layers) - 1):
+                layers.append(nn.Linear(config.hidden_layers[i], config.hidden_layers[i + 1]))
+                if config.batch_normalization:
+                    layers.append(nn.BatchNorm1d(config.hidden_layers[i + 1]))
+                layers.append(nn.ReLU())
+                layers.append(nn.Dropout(config.dropout_rate))
+
+            # Add the output layer
+            layers.append(nn.Linear(config.hidden_layers[-1], config.output_shape))
+            if config.final_activation == "sigmoid":
+                layers.append(nn.Sigmoid())
+            elif config.final_activation == "softmax":
+                layers.append(nn.Softmax(dim=1))  # Make sure to specify the appropriate dimension for softmax
+        else:
+            layers.append(nn.Linear(config.input_shape, config.output_shape))
+            if config.final_activation == "sigmoid":
+                layers.append(nn.Sigmoid())
+            elif config.final_activation == "softmax":
+                layers.append(nn.Softmax(dim=1))  # Make sure to specify the appropriate dimension for softmax
+
+        # Combine all layers into the MLP
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.mlp(x)
---
+++ model-suite/models/mlp/model_config.py
@@ -0,0 +1,27 @@
+
+import warnings
+
+class MultiLayerPerceptronConfig:
+    def __init__(
+        self,
+        dropout_rate = 0.2,
+        input_shape = 400,
+        output_shape = 10,
+        hidden_layers: tuple or False = (200,),
+        activation = "relu",
+        final_activation = "sigmoid",
+        batch_normalization = True,
+    ):
+
+        self.dropout_rate = dropout_rate
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.hidden_layers = hidden_layers
+        self.activation = activation
+        self.final_activation = final_activation
+        self.batch_normalization = batch_normalization
+
+        if not hidden_layers:
+            warnings.warn("Since there are no hidden layers the activation function will not be used.")
+
+# 400 200 3
