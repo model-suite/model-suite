c7e74856a87f73b5e28ac2af938fc1d83d792742
osbm <osmanfbayram@gmail.com>
2023-08-15 03:16:14 +0300

    Add apache license

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="model-suite",
     packages=find_packages(exclude=[]),
     version="0.0.1",
+    license='Apache License 2.0',
     description="Model Suite",
     url="https://github.com/model-suite/model-suite",
     install_requires=["torch"],
