3040a6a5f4ddf3613c81d8aeafe019c433b144fe
osbm <osmanfbayram@gmail.com>
2023-08-25 01:03:22 +0300

    add model saving and loading

--- model_suite/models/mlp/model_config.py
+++ model_suite/models/mlp/model_config.py
@@ -1,7 +1,8 @@
 import warnings
+from ...base_config import BaseModelConfig
 
-
-class MultiLayerPerceptronConfig:
+class MultiLayerPerceptronConfig(BaseModelConfig):
+    architecture_name = "MultiLayerPerceptron"
     def __init__(
         self,
         dropout_rate: float = 0.2,
@@ -11,6 +12,7 @@ class MultiLayerPerceptronConfig:
         activation: str = "relu",
         final_activation: str = "sigmoid",
         batch_normalization: bool = True,
+        **kwargs
     ):
         self.dropout_rate = dropout_rate
         self.input_shape = input_shape
