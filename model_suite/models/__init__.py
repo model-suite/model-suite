11cfd82bf4604424e2b47f400316c6a94622f91c
osbm <osmanfbayram@gmail.com>
2023-08-26 17:38:18 +0300

    Add autoencoder model

--- model_suite/models/__init__.py
+++ model_suite/models/__init__.py
@@ -1,3 +1,4 @@
 from .mlp import MultiLayerPerceptron, MultiLayerPerceptronConfig
 from .vgg import VGG, VGGConfig
 from .alexnet import AlexNet, AlexNetConfig
+from .ae import Autoencoder, AutoencoderConfig
