68e811754080a7df4f199834c35d922bf28ec87e
osbm <osmanfbayram@gmail.com>
2024-01-11 14:58:53 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="model-suite",
     packages=find_packages(exclude=[]),
-    version="0.0.2",
+    version="0.0.3",
     license="Apache License 2.0",
     description="Model Suite",
     url="https://github.com/model-suite/model-suite",
