56583f1e4275781873d12d3863741fc3de6a3afe
osbm <osmanfbayram@gmail.com>
2024-01-11 16:51:48 +0300

    simplify the model

--- model_suite/models/unet/model_architecture.py
+++ model_suite/models/unet/model_architecture.py
@@ -1,88 +1,104 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from .model_config import UNetConfig
 from ...base_model import BaseModel
 
 __all__ = ["UNet"]
 
 
-class UNet(BaseModel):
-    def __init__(self, config: UNetConfig):
-        self.config = config
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False), # no bias because batchnorm cancels it
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
 
-        if self.config.spatial_dimensions == 2:
-            self.__conv = nn.Conv2d
-            self.__max_pool = nn.MaxPool2d
-            self.__batch_norm = nn.BatchNorm2d
-            self.__conv_transpose = nn.ConvTranspose2d
-        elif self.config.spatial_dimensions == 3:
-            self.__conv = nn.Conv3d
-            self.__max_pool = nn.MaxPool3d
-            self.__batch_norm = nn.BatchNorm3d
-            self.__conv_transpose = nn.ConvTranspose3d
-
-        self.inc = self._get_double_conv(
-            self.config.in_channels, self.config.channels[0]
-        )
-        self.down1 = self.__get_down(self.config.in_channels, self.config.channels[0])
-        self.down2 = self.__get_down(self.config.channels[0], self.config.channels[1])
-        self.down3 = self.__get_down(self.config.channels[1], self.config.channels[2])
-
-        self.up1 = self.__get_up(self.config.channels[3], self.config.channels[2])
-        self.up2 = self.__get_up(self.config.channels[2], self.config.channels[1])
-        self.up3 = self.__get_up(self.config.channels[1], self.config.channels[0])
-        self.outc = self.__conv(self.config.channels[0], self.config.out_channels, 1)
-
-
-    def _get_double_conv(self, in_channels: int, out_channels: int) -> nn.Sequential:
-        return nn.Sequential(
-            self.__conv(in_channels, out_channels, self.config.kernel_size),
-            self.__batch_norm(out_channels),
-            nn.ReLU(inplace=True),
-            self.__conv(out_channels, out_channels, self.config.kernel_size),
-            self.__batch_norm(out_channels),
-            nn.ReLU(inplace=True),
-        )
-
-    def __get_down(self, in_channels: int, out_channels: int) -> nn.Sequential:
-        return nn.Sequential(
-            self.__max_pool(self.config.strides),
-            self._get_double_conv(in_channels, out_channels),
-        )
-
-    def __get_up(self, in_channels: int, out_channels: int) -> nn.Sequential:
-        if self.config.is_bilinear:
-            return nn.Sequential(
-                nn.Upsample(scale_factor=self.config.strides, mode="bilinear"),
-                self._get_double_conv(in_channels, out_channels),
-            )
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
-            return nn.Sequential(
-                self.__conv_transpose(
-                    in_channels, out_channels, self.config.strides, self.config.strides
-                ),
-                self._get_double_conv(in_channels, out_channels),
-            )
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
 
-    def __get_out_conv(self, in_channels: int, out_channels: int) -> nn.Sequential:
-        return nn.Sequential(
-            self.__conv(in_channels, out_channels, 1), nn.Softmax(dim=1)
-        )
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
 
-    def forward(x):
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet(BaseModel):
+    def __init__(self, config: UNetConfig):
+        super().__init__()
+        self.config = config
+
+        self.inc = DoubleConv(self.config.in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if self.config.bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, self.config.bilinear)
+        self.up2 = Up(512, 256 // factor, self.config.bilinear)
+        self.up3 = Up(256, 128 // factor, self.config.bilinear)
+        self.up4 = Up(128, 64, self.config.bilinear)
+        self.outc = OutConv(64, self.config.out_channels)
+
+    def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x = self.up1(x4)
-        x = self.up2(x)
-        x = self.up3(x)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
         logits = self.outc(x)
-
-        if self.config.out_activation == "softmax":
-            logits = nn.Softmax(dim=1)(logits)
-        elif self.config.out_activation == "sigmoid":
-            logits = nn.Sigmoid()(logits)
-
         return logits
+
+if __name__ == "__main__":
+    config = UNetConfig(
+        in_channels=3,
+        out_channels=2,
+        bilinear=True,
+    )
+    model = UNet(config)
+    print(model)
+    print(model(torch.randn(1, 3, 128, 128)).shape)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="model-suite",
     packages=find_packages(exclude=[]),
-    version="0.0.4",
+    version="0.0.5",
     license="Apache License 2.0",
     description="Model Suite",
     url="https://github.com/model-suite/model-suite",
